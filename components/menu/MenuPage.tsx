f2df101de84b5a2a260e0c4c4c5a01be881f8882
pavsumin <pavsumin@gmail.com>
2026-02-27 16:57:20 +0700

    Menu tabs category bug fixed

--- components/menu/MenuPage.tsx
+++ components/menu/MenuPage.tsx
@@ -52,7 +52,7 @@ export default function MenuPage() {
 			</section>
 
 			{/* Sticky Categories */}
-			<div className='sticky top-0 z-50 bg-[#0e2e1c]/90 backdrop-blur-md border-b border-white/10'>
+			<div className='sticky top-[76px] z-30 bg-[#0e2e1c]/80 backdrop-blur-md border-b border-white/10'>
 				<div className='max-w-[1280px] mx-auto px-6 py-5 flex gap-6 overflow-x-auto scrollbar-hide'>
 					{menuData.map(category => (
 						<button
