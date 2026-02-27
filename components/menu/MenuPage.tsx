68c1b935bab140344cb3b990d754afddd8497ff8
pavsumin <pavsumin@gmail.com>
2026-02-27 20:17:47 +0700

    Menu card Image has onError /fallback.png

--- components/menu/MenuPage.tsx
+++ components/menu/MenuPage.tsx
@@ -154,6 +154,9 @@ export default function MenuPage() {
 												loading='lazy'
 												sizes='(max-width: 768px) 100vw, 33vw'
 												className='object-cover group-hover:scale-105 transition-transform duration-500'
+												onError={e => {
+													e.currentTarget.src = '/fallback.png'
+												}}
 											/>
 										</div>
 
