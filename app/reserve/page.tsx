eb4033114a1273114db294c8e4e6710a28b43384
pavsumin <pavsumin@gmail.com>
2026-02-28 09:05:04 +0700

    Reservation form has time hours and date only today and next days

--- app/reserve/page.tsx
+++ app/reserve/page.tsx
@@ -6,6 +6,20 @@ export default function ReservePage() {
 	const [loading, setLoading] = useState(false)
 	const [success, setSuccess] = useState(false)
 	const [error, setError] = useState(false)
+	const [selectedDate, setSelectedDate] = useState<string>('')
+
+	const today = new Date().toISOString().split('T')[0]
+
+	function getMaxTime(date: string) {
+		if (!date) return '21:00'
+
+		const day = new Date(date).getDay()
+
+		// 6 = Saturday
+		if (day === 6) return '20:00'
+
+		return '21:00'
+	}
 
 	async function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
 		e.preventDefault()
@@ -35,7 +49,7 @@ export default function ReservePage() {
 			if (!res.ok) throw new Error('Failed')
 
 			setSuccess(true)
-			form.reset() // ✅ теперь безопасно
+			form.reset() // теперь безопасно
 		} catch (err) {
 			console.error(err)
 			setError(true)
@@ -45,39 +59,44 @@ export default function ReservePage() {
 	}
 
 	return (
-		<main className='min-h-screen bg-[#0e2e1c] text-white flex items-center justify-center px-6 py-34'>
-			<div className='w-full max-w-lg bg-white/5 border border-white/10 rounded-2xl p-8 backdrop-blur-md'>
-				<h1 className='text-3xl font-bold text-[#d4af37] mb-8 text-center'>
+		<main className='min-h-screen bg-[#0e2e1c] text-white flex items-center justify-center px-4 py-20 md:py-32'>
+			<div className='w-full max-w-lg bg-white/5 border border-white/10 rounded-xl md:rounded-2xl p-6 md:p-8 backdrop-blur-md'>
+				<h1 className='text-2xl md:text-3xl font-bold text-[#d4af37] mb-6 md:mb-8 text-center'>
 					Reserve a Table
 				</h1>
 
-				<form onSubmit={handleSubmit} className='space-y-5'>
+				<form onSubmit={handleSubmit} className='space-y-4 md:space-y-5'>
 					<input
 						name='name'
 						required
 						placeholder='Full Name'
-						className='w-full px-4 py-3 bg-white/10 rounded-lg outline-none focus:ring-2 focus:ring-[#d4af37]'
+						className='w-full text-base px-4 py-3 bg-white/10 rounded-lg outline-none focus:ring-2 focus:ring-[#d4af37]'
 					/>
 
 					<input
 						name='phone'
 						required
 						placeholder='Phone Number'
-						className='w-full px-4 py-3 bg-white/10 rounded-lg outline-none focus:ring-2 focus:ring-[#d4af37]'
+						className='w-full text-base px-4 py-3 bg-white/10 rounded-lg outline-none focus:ring-2 focus:ring-[#d4af37]'
 					/>
 
 					<input
 						type='date'
 						name='date'
 						required
-						className='w-full px-4 py-3 bg-white/10 rounded-lg outline-none focus:ring-2 focus:ring-[#d4af37]'
+						min={today}
+						value={selectedDate}
+						onChange={e => setSelectedDate(e.target.value)}
+						className='w-full text-base px-4 py-3 bg-white/10 rounded-lg outline-none focus:ring-2 focus:ring-[#d4af37]'
 					/>
 
 					<input
 						type='time'
 						name='time'
 						required
-						className='w-full px-4 py-3 bg-white/10 rounded-lg outline-none focus:ring-2 focus:ring-[#d4af37]'
+						min='10:30'
+						max={getMaxTime(selectedDate)}
+						className='w-full text-base px-4 py-3 bg-white/10 rounded-lg outline-none focus:ring-2 focus:ring-[#d4af37]'
 					/>
 
 					<input
@@ -86,41 +105,38 @@ export default function ReservePage() {
 						required
 						min={1}
 						placeholder='Guests'
-						className='w-full px-4 py-3 bg-white/10 rounded-lg outline-none focus:ring-2 focus:ring-[#d4af37]'
+						className='w-full text-base px-4 py-3 bg-white/10 rounded-lg outline-none focus:ring-2 focus:ring-[#d4af37]'
 					/>
 
 					<button
 						type='submit'
 						disabled={loading}
 						className='
-              w-full
-              px-8 py-3
-              bg-gradient-to-r from-[#d4af37] to-[#f2d47a]
-              text-black
-              rounded-full
-              shadow-[0_8px_30px_rgba(212,175,55,0.4)]
-              hover:brightness-110
-              hover:scale-101
-							cursor-pointer
-              transition-all
-              duration-300
-              font-medium
-              disabled:opacity-50
-              disabled:cursor-not-allowed
-            '
+          w-full
+          mt-2
+          px-8
+          py-3
+          bg-gradient-to-r
+          from-[#d4af37]
+          to-[#f2d47a]
+          text-black
+          rounded-full
+          shadow-[0_8px_30px_rgba(212,175,55,0.4)]
+          hover:brightness-110
+          hover:scale-101
+					cursor-pointer
+          transition-all
+          duration-300
+          font-medium
+          disabled:opacity-50
+        '
 					>
 						{loading ? 'Sending...' : 'Reserve Now'}
 					</button>
 
 					{success && (
-						<p className='text-green-400 text-center mt-4'>
+						<p className='text-green-400 text-center mt-3'>
 							Reservation sent successfully!
-						</p>
-					)}
-
-					{error && (
-						<p className='text-red-400 text-center mt-4'>
-							Something went wrong. Please try again.
 						</p>
 					)}
 				</form>
